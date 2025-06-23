07aaadc17da615129566e559e7e655183685aa35
Daniel Vogelnest <daniel.vogelnest@me.com>
2025-06-23 11:24:12 +1000

    Improve and consolidate service logging

--- tv-service/src/index.ts
+++ tv-service/src/index.ts
@@ -1,8 +1,7 @@
 import Service from "webos-service";
 import { LgTvMqtt, LgTvMqttConfig } from "./lgtv-mqtt";
 
-const service = new Service("com.danvnest.applauncher+mqtt.service"); // Create service by service name on package.json
-
+const service = new Service("com.danvnest.applauncher+mqtt.service");
 const config: LgTvMqttConfig = {
   host: "YOUR MQTT BROKER HOST",
   port: 1883,
@@ -12,13 +11,6 @@ const config: LgTvMqttConfig = {
 };
 
 const lgTvMqtt = new LgTvMqtt(service, config);
-
 service.register("start", (message) => lgTvMqtt.start(message));
-
-service.register("stop", (message) => lgTvMqtt.stop(message));
-
-service.register("logs", (message) => lgTvMqtt.logs(message));
-
-service.register("clearLogs", (message) => lgTvMqtt.clearLogs(message));
-
 service.register("getState", (message) => lgTvMqtt.getState(message));
+service.register("getLogs", (message) => lgTvMqtt.getLogs(message));
--- tv-service/src/lgtv-mqtt.ts
+++ tv-service/src/lgtv-mqtt.ts
@@ -1,6 +1,5 @@
 import { Client, connect, IClientOptions, IClientPublishOptions } from "mqtt";
 import Service, { Message } from "webos-service";
-import { Logging } from "./logging";
 
 enum ServiceState {
   STARTED = "STARTED",
@@ -9,7 +8,6 @@ enum ServiceState {
   FAILED_TO_SET_ONLINE = "FAILED TO SET ONLINE",
   FAILED_TO_START = "FAILED TO START",
   STOPPED = "STOPPED",
-  FAILED_TO_STOP = "FAILED TO STOP",
 }
 
 interface ForegroundAppInfo {
@@ -40,25 +38,29 @@ export interface LgTvMqttConfig {
   deviceID: string; // This should be unique across the MQTT network. If you're using this on multiple TVs, update this
 }
 
+class Logging {
+  private logs: string[] = [];
+  log(...s: any[]) {
+    this.logs.unshift(`${new Date().toISOString()} - ${JSON.stringify(s)}`);
+  }
+  getLogs() {
+    return this.logs;
+  }
+}
+
 export class LgTvMqtt {
   constructor(private service: Service, private config: LgTvMqttConfig) {}
-
   private state: ServiceState = ServiceState.STOPPED;
   private logging = new Logging();
   private keepAlive: Record<string, any> = {};
   private client: Client | undefined;
-
   private clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
   private connectUrl = `mqtt://${this.config.host}:${this.config.port}`;
-
-  // MQTT auto-discovery configuration
   private topicAutoDiscoveryPlayState = `homeassistant/sensor/${this.config.deviceID}/playState/config`;
   private topicAutoDiscoveryAppId = `homeassistant/sensor/${this.config.deviceID}/appId/config`;
   private topicAutoDiscoveryType = `homeassistant/sensor/${this.config.deviceID}/type/config`;
-
   private topicAvailability = `LGTV2MQTT/${this.config.deviceID}/availability`;
   private topicState = `LGTV2MQTT/${this.config.deviceID}/state`;
-
   private mqttConfig: IClientOptions = {
     clientId: this.clientId,
     clean: true,
@@ -75,257 +77,136 @@ export class LgTvMqtt {
     },
   };
 
-  private playStateConfig = JSON.stringify(
-    this.createAutoDiscoveryConfig("mdi:play-pause", "play", "Play State")
-  );
-  private appIdConfig = JSON.stringify(
-    this.createAutoDiscoveryConfig("mdi:apps", "app", "Application ID")
-  );
-  private typeConfig = JSON.stringify(
-    this.createAutoDiscoveryConfig("mdi:import", "type", "Discovery Type")
-  ); // Do we need these?
-
-  private publicOptionRetain: IClientPublishOptions = { qos: 0, retain: true };
-  private publicOptionNotRetain: IClientPublishOptions = {
-    qos: 0,
-    retain: false,
-  };
+  private playStateConfig = JSON.stringify(this.createAutoDiscoveryConfig("mdi:play-pause", "play", "Play State"));
+  private appIdConfig = JSON.stringify(this.createAutoDiscoveryConfig("mdi:apps", "app", "Application ID"));
+  private typeConfig = JSON.stringify(this.createAutoDiscoveryConfig("mdi:import", "type", "Discovery Type"));
+  private publishOptionRetain: IClientPublishOptions = { qos: 0, retain: true };
+  private publishOptionNoRetain: IClientPublishOptions = { qos: 0, retain: false };
 
   start(message: Message) {
     if (this.state === ServiceState.STARTED) {
       return;
     }
-
     try {
-      this.logging.log("Starting LG TV 2 MQTT service...");
-
-      // This tells the ActivityManager to keep the service running in the background, so the MQTT connection will be kept alive.
+      this.logging.log("Starting the LGTV MQTT connection service");
       this.service.activityManager.create("keepAlive", (activity) => {
-        this.logging.log("KeepAlive created");
         this.keepAlive = activity;
       });
-      this.logging.log("Registered keepAlive");
-
-      // Connect to the MQTT broker
-
+      this.logging.log("Service set to maintain the connection in the background");
       this.logging.log(
-        `Connecting to MQTT server with: ${JSON.stringify(this.mqttConfig)}`
+        `Connecting to the MQTT server with the following settings:\n${JSON.stringify(this.mqttConfig)}`
       );
-
       this.client = connect(this.connectUrl, this.mqttConfig);
-
-      this.logging.log("Connected to mqtt server");
-
+      this.logging.log("Successfully connected to the MQTT server");
       this.sendAutoDiscovery(message);
-
       this.publishInitialState(message);
-
       this.publishAvailability(message);
-
       this.logging.log("Subscribing to media service");
-
-      // Subscribe to the com.webos.media service, to receive updates from the tv
       this.service
         .subscribe("luna://com.webos.media/getForegroundAppInfo", {
           subscribe: true,
         })
         .on("response", (message: Message) =>
-          this.handleForegroundAppResponse(
-            message,
-            message.payload as ForegroundAppIdResponse
-          )
+          this.handleForegroundAppResponse(message, message.payload as ForegroundAppIdResponse)
         );
-
-      this.logging.log("Started LG TV 2 MQTT service");
-
-      this.respond(message, { started: true });
-
+      this.logging.log("Service started successfully - reporting media state via MQTT");
+      message.respond({ started: true });
       this.state = ServiceState.STARTED;
-    } catch (err) {
-      this.logging.log(`Failed starting: ${JSON.stringify(err)}`);
-      this.respond(message, { started: false });
+    } catch (error) {
+      this.logging.log(`Failed to start LGTV MQTT connection service:\n${JSON.stringify(error)}`);
+      this.logging.log("Service will attemp to start again in 10 seconds");
+      message.respond({ started: false });
       this.state = ServiceState.FAILED_TO_START;
+      setTimeout(() => this.start(message), 10000);
     }
   }
 
-  private respond(message: Message, payload: Record<string, any>) {
-    message.respond({
-      ...payload,
-      logs: this.logging.getLogs(),
-    });
+  getState(message: Message) {
+    message.respond({ state: this.state });
   }
 
-  private handleForegroundAppResponse(
-    message: Message,
-    payload: ForegroundAppIdResponse
-  ) {
-    if (payload && payload.foregroundAppInfo) {
-      if (
-        Array.isArray(payload.foregroundAppInfo) &&
-        payload.foregroundAppInfo.length > 0
-      ) {
-        this.logging.log(
-          `Send ForegroundAppInfo update to MQTT: ${JSON.stringify(payload)}`
-        );
+  getLogs(message: Message) {
+    message.respond({ logs: this.logging.getLogs() });
+  }
 
-        const info: ForegroundAppInfo = payload.foregroundAppInfo[0];
-
-        const state = this.createState(info.playState, info.appId, info.type);
-
-        this.client?.publish(
-          this.topicState,
-          JSON.stringify(state),
-          this.publicOptionNotRetain
-        );
-      } else {
-        this.logging.log(
-          `Ignored ForegroundAppInfo because it's no array, or empty: ${JSON.stringify(
-            payload
-          )}`
-        );
-        this.client?.publish(
-          this.topicState,
-          JSON.stringify(this.createState("idle", "unknown", "unknown")),
-          this.publicOptionNotRetain
-        );
-      }
+  private handleForegroundAppResponse(message: Message, payload: ForegroundAppIdResponse) {
+    if (
+      payload &&
+      payload.foregroundAppInfo &&
+      Array.isArray(payload.foregroundAppInfo) &&
+      payload.foregroundAppInfo.length > 0
+    ) {
+      this.logging.log(`Sending foreground app state update:\n${JSON.stringify(payload)}`);
+      const info: ForegroundAppInfo = payload.foregroundAppInfo[0];
+      const state = this.createState(info.playState, info.appId, info.type);
+      this.client?.publish(this.topicState, JSON.stringify(state), this.publishOptionNoRetain);
     } else {
-      this.logging.log(
-        `Ignored ForegroundAppInfo because it contains no info: ${JSON.stringify(
-          message
-        )}`
-      );
+      this.logging.log(`WARNING: Unexpected foreground app update:\n${JSON.stringify(message)}`);
       this.client?.publish(
         this.topicState,
         JSON.stringify(this.createState("idle", "unknown", "unknown")),
-        this.publicOptionNotRetain
+        this.publishOptionNoRetain
       );
     }
-
     this.publishAvailability(message);
   }
 
   private sendAutoDiscovery(message: Message) {
-    this.logging.log("Sending Home Assistant auto-discovery configs..");
-    // Send the Home Assistant auto-discovery configs
+    this.logging.log("Sending Home Assistant auto-discovery configs");
     try {
-      this.client?.publish(
-        this.topicAutoDiscoveryPlayState,
-        this.playStateConfig,
-        this.publicOptionRetain,
-        (err) => this.handleError(err, this.topicAutoDiscoveryPlayState)
-      );
-
-      this.client?.publish(
-        this.topicAutoDiscoveryAppId,
-        this.appIdConfig,
-        this.publicOptionRetain,
-        (err) => this.handleError(err, this.topicAutoDiscoveryAppId)
-      );
-
-      this.client?.publish(
-        this.topicAutoDiscoveryType,
-        this.typeConfig,
-        this.publicOptionRetain,
-        (err) => this.handleError(err, this.topicAutoDiscoveryType)
-      );
-    } catch (err) {
-      this.logging.log(
-        "Failed to publish Home Assistant Auto Discovery configs",
-        `${JSON.stringify(err)}`
-      );
-      this.respond(message, { started: false });
+      const discoveryConfig = [
+        { topic: this.topicAutoDiscoveryPlayState, config: this.playStateConfig },
+        { topic: this.topicAutoDiscoveryAppId, config: this.appIdConfig },
+        { topic: this.topicAutoDiscoveryType, config: this.typeConfig },
+      ];
+      discoveryConfig.forEach(({ topic, config }) => {
+        this.client?.publish(topic, config, this.publishOptionRetain, (error) => this.handlePublishEnd(error, topic));
+      });
+    } catch (error) {
+      this.logging.log(`Failed to publish Home Assistant auto-discovery configs, error:\n${JSON.stringify(error)}`);
+      message.respond({ started: false });
       this.state = ServiceState.FAILED_TO_PUBLISH_CONFIGS;
-      throw err;
+      throw error;
     }
   }
 
   private publishInitialState(message: Message) {
-    // Publish initial state
+    this.logging.log("Sending initial TV state");
     try {
-      let pubOptions: IClientPublishOptions = { qos: 0, retain: false };
       this.client?.publish(
         this.topicState,
         JSON.stringify(this.createState("idle", "unknown", "unknown")),
-        pubOptions,
-        (err) => this.handleError(err, this.topicState)
+        this.publishOptionNoRetain,
+        (error) => this.handlePublishEnd(error, this.topicState)
       );
-    } catch (err) {
-      this.logging.log(
-        "Failed to send initial MQTT state",
-        `${JSON.stringify(err)}`
-      );
-      this.respond(message, { started: false });
+    } catch (error) {
+      this.logging.log(`Failed to send initial TV state, error:\n${JSON.stringify(error)}`);
+      message.respond({ started: false });
       this.state = ServiceState.FAILED_TO_PUBLISH_INITIAL_STATE;
-      throw err;
+      throw error;
     }
   }
 
   private publishAvailability(message: Message) {
-    // Set availability to online
+    this.logging.log("Sending notification of availability");
     try {
-      this.client?.publish(
-        this.topicAvailability,
-        "online",
-        this.publicOptionRetain,
-        (err) => this.handleError(err, this.topicAvailability)
+      this.client?.publish(this.topicAvailability, "online", this.publishOptionRetain, (error) =>
+        this.handlePublishEnd(error, this.topicAvailability)
       );
-    } catch (err) {
-      this.logging.log(
-        "Failed to set availability to online",
-        `${JSON.stringify(err)}`
-      );
-      this.respond(message, { started: false });
+    } catch (error) {
+      this.logging.log(`Failed to notify of availability, error:\n${JSON.stringify(error)}`);
+      message.respond({ started: false });
       this.state = ServiceState.FAILED_TO_SET_ONLINE;
-      throw err;
+      throw error;
     }
   }
 
-  private handleError(err: Error | undefined, topicName: string) {
-    if (err) {
-      this.logging.log(
-        `An error occurred during publish to ${topicName}`,
-        `${JSON.stringify(err)}`
-      );
+  private handlePublishEnd(error: Error | undefined, topicName: string) {
+    if (error) {
+      this.logging.log(`An error occurred during publish to ${topicName}`, `${JSON.stringify(error)}`);
     } else {
       this.logging.log(`Published successfully to ${topicName}`);
     }
-  }
-
-  stop(message: Message) {
-    try {
-      this.logging.log("Stopping service");
-
-      this.logging.log("Closing MQTT connection");
-      this.client?.end();
-
-      this.logging.log("Complete keepAlive");
-
-      // When you're done, complete the activity
-      this.service.activityManager.complete(this.keepAlive);
-
-      this.logging.log("Completed keepAlive");
-
-      this.respond(message, { stopped: true });
-      this.state = ServiceState.STOPPED;
-    } catch (err) {
-      this.logging.log(`Failed stopping: ${JSON.stringify(err)}`);
-      this.respond(message, { started: false });
-      this.state = ServiceState.FAILED_TO_STOP;
-    }
-  }
-
-  logs(message: Message) {
-    this.respond(message, {});
-  }
-
-  clearLogs(message: Message) {
-    this.logging.clearLogs();
-    this.respond(message, { cleared: true });
-  }
-
-  getState(message: Message) {
-    this.respond(message, { state: this.state });
   }
 
   private createAutoDiscoveryConfig(icon: string, id: string, name: string) {
