55c29bc715e3c04ea6ac3c0fc2a70a4721e0b11e
Daniel Vogelnest <daniel.vogelnest@me.com>
2025-06-27 22:28:38 +1000

    Exit script with error code

--- package-and-install.sh
+++ package-and-install.sh
@@ -1,29 +1,29 @@
 service_changes=true
 checksum_file="tv-service-src/.last_checksum"
 if [ -f "$checksum_file" ]; then
-    current_checksum=$(find tv-service-src -type f ! -name '.last_checksum' -exec md5sum {} + | sort | md5sum | awk '{print $1}') || exit
+    current_checksum=$(find tv-service-src -type f ! -name '.last_checksum' -exec md5sum {} + | sort | md5sum | awk '{print $1}') || exit 1
     last_checksum=$(cat "$checksum_file")
     if [ "$current_checksum" = "$last_checksum" ]; then
         service_changes=false
     fi
 fi
-echo "$current_checksum" >"$checksum_file" || exit
+echo "$current_checksum" >"$checksum_file" || exit 1
 
 if [ $service_changes = true ]; then
     echo "Compiling tv-service"
-    cd tv-service-src || exit
+    cd tv-service-src || exit 1
     echo "> npm install && npm run build && cp package.json ../tv-service/ && cd ../tv-service && npm install --omit=dev"
-    npm install || exit
-    npm run build || exit
-    cp package.json ../tv-service/ || exit
-    cd ../tv-service && npm install --omit=dev || exit
-    cd .. || exit
+    npm install || exit 1
+    npm run build || exit 1
+    cp package.json ../tv-service/ || exit 1
+    cd ../tv-service && npm install --omit=dev || exit 1
+    cd .. || exit 1
     echo "\n"
 fi
 
 echo "Packaging tv-app and tv-service"
 echo "> ares-package tv-app/ tv-service/\n"
-ares-package tv-app/ tv-service/ || exit
+ares-package tv-app/ tv-service/ || exit 1
 
 package_file="com.danvnest.applauncher+mqtt_1.1.0_all.ipk"
 timeout=5000
@@ -34,7 +34,7 @@ while [ ! -e $package_file ] && [ $elapsed -lt $timeout ]; do
 done
 if [ ! -e $package_file ]; then
     echo "ERROR: $package_file was not created"
-    exit
+    exit 1
 fi
 
 echo "\nInstalling on TV"
